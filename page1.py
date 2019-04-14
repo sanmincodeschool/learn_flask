7c0a7b1131fe97ede4a083af3b74f4e765b9b5d8
karlgao <karlgaoyuan@qq.com>
2019-04-14 12:21:12 +0800

    flask 模板学习

--- page1.py
+++ page1.py
@@ -1,7 +1,14 @@
 #-*- coding: utf8 -*-
-from flask import Blueprint,jsonify,session
+from flask import Blueprint,jsonify,session,render_template
 
 page1 = Blueprint('page1',__name__,url_prefix='/page1')
+
+
+@page1.context_processor
+def common():
+    return{
+        'isLogin' : True
+    }
 
 @page1.route('/a/')
 def test():
@@ -14,3 +21,11 @@ def test():
         "data" : "session"
     }
     return jsonify(data)
+
+@page1.route('/main/')
+def main():
+    data = {
+        "name" : u'数据',
+        "isLogin" : True
+    }
+    return render_template('main.html',**data)
